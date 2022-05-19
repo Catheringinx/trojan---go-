6406b61a26f0ab06dd8a70375d9c9b0a8fc9716d
jonssonyan <yz808@outlook.com>
2022-05-19 10:25:23 +0800

    fix: bug

--- install_script.sh
+++ install_script.sh
@@ -1411,11 +1411,11 @@ function updateTrojanPanel() {
   done
 
   if [[ ${mariadb_ip} == "trojan-panel-mariadb" ]];then
-    docker exec trojan-panel-mariadb mysql -u ${mariadb_user} -p ${mariadb_pas} -e 'drop database trojan_panel_db;'
-    docker exec trojan-panel-mariadb mysql -u ${mariadb_user} -p ${mariadb_pas} -e 'create database trojan_panel_db;'
+    docker exec trojan-panel-mariadb mysql -u${mariadb_user} -p${mariadb_pas} -e 'drop database trojan_panel_db;'
+    docker exec trojan-panel-mariadb mysql -u${mariadb_user} -p${mariadb_pas} -e 'create database trojan_panel_db;'
   else
-    docker exec trojan-panel-mariadb mysql -h ${mariadb_ip} -u ${mariadb_user} -p ${mariadb_pas} -e 'drop database trojan_panel_db;'
-    docker exec trojan-panel-mariadb mysql -h ${mariadb_ip} -u ${mariadb_user} -p ${mariadb_pas} -e 'create database trojan_panel_db;'
+    docker exec trojan-panel-mariadb mysql -h${mariadb_ip} -u${mariadb_user} -p${mariadb_pas} -e 'drop database trojan_panel_db;'
+    docker exec trojan-panel-mariadb mysql -h${mariadb_ip} -u${mariadb_user} -p${mariadb_pas} -e 'create database trojan_panel_db;'
   fi
 
   read -r -p '请输入Redis的IP地址(默认:本机Redis): ' redis_host
